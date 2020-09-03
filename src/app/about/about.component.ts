ce6b5f9fe46af7d32947ae8f581c521ad29521f9
stefan521 <radu11.sm@gmail.com>
2020-09-03 21:37:04 +0100

    Reactive component.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { interval, timer, fromEvent, Observable, noop } from 'rxjs';
+
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
@@ -12,80 +13,7 @@ export class AboutComponent implements OnInit {
   }
 
   ngOnInit() {
-    // RxJs = Reactive Extensions for JavaScript
 
-    /* Stream Lesson
-
-    // stream of values that are being emitted over time
-    document.addEventListener('click', evt => {
-      console.log(evt);
-    }); // multivalue stream that never completes
-
-    let counter = 0;
-
-    // a second stream of values
-    setInterval(() => {
-
-      console.log(counter);
-      counter++;
-    
-    }, 1000); // multivalue stream that never completes
-
-
-    // a special type of stream that only emits one value and then completes
-    setTimeout(() => {
-      console.log("finished....");
-    }, 3000);
-
-    */
-
-
-    /* Observable lesson
-
-    const interval$ = timer(3000, 1000); // Observable emitting numbers a definition for a stream of values
-
-    // an Observable will only become a stream if we subscribe to it
-    const sub = interval$.subscribe(val => console.log('stream 1 ' + val));
-    // interval$.subscribe(val => console.log('stream 2 ' + val));
-
-    const click$ = fromEvent(document, 'click');
-
-    // the observable emits its values and either errors out or completes. if it errors it means it won't ever complete. values can't be emitted after errors or completion
-    click$.subscribe(
-      evt => console.log(evt), // value callback for the stream values
-      err => console.log(err), // error handler
-      () => console.log("stream completed") // callback for completed stream
-    );
-
-    setTimeout(() => sub.unsubscribe(), 5000);
-
-    */
-
-    const http$: Observable<any> = Observable.create(observer => {
-      // we use the observable internally to implement the observable
-      // observer.next()
-      // observer.error()
-      // observer.complete()
-
-      fetch('/api/courses')
-        .then(response => {
-          return response.json();
-        })
-        .then(body => {
-          observer.next(body);
-
-          observer.complete(); // can't call next anymore and can't error
-        })
-        .catch(err => {
-          observer.error(err); // can't complete and can't emit new values anymore
-        });
-    });
-
-    http$.subscribe(
-      courses => console.log(courses),
-      noop,
-      () => console.log('completed')
-    );
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
+import {interval, Observable, of, timer, noop} from 'rxjs';
 import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+
+import { createHttpObservable } from '../common/util';
 
 
 @Component({
@@ -11,6 +13,9 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
+    beginnersCourses$: Observable<Course[]>;
+
+    advancedCourses$: Observable<Course[]>;
 
     constructor() {
 
@@ -18,8 +23,33 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
 
+        const http$ = createHttpObservable('/api/courses');
+        
+        const courses$: Observable<Course[]> = http$
+            .pipe(
+                tap(() => console.log("http request executed")), // produces side effects in the observable chain - logging, updates outside of this chain etc
+                map(response => Object.values(response["payload"])),
+                shareReplay()
+            );
 
+        this.beginnersCourses$ = courses$
+            .pipe(
+                map(courses => courses
+                    .filter(course => course.category === 'BEGINNER')
+                )
+            );
 
+        this.advancedCourses$ = courses$
+            .pipe(
+                map(courses => courses
+                    .filter(course => course.category === 'ADVANCED'))
+            )
+
+        courses$.subscribe(
+            noop,
+            noop,
+            () => console.log('completed')
+        );
     }
 
 }
